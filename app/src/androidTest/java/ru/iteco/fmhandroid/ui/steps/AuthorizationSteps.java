d429e0fcee8d93ad5cb61adffa141abbe15b1c88
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-09 10:44:08 +0400

    Update AuthorizationSteps.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AuthorizationSteps.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AuthorizationSteps.java
@@ -12,25 +12,30 @@ import ru.iteco.fmhandroid.ui.elements.CommonElements;
 import ru.iteco.fmhandroid.ui.elements.MainScreen;
 
 public class AuthorizationSteps {
+    Allure.step("Проверка, что это экран авторизации");
     AuthorizationScreen AuthorizationScreen = new AuthorizationScreen();
     CommonElements CommonElements = new CommonElements();
     MainScreen MainScreen = new MainScreen();
 
     public void isAuthorizationScreen() {
+        Allure.step("Проверка, что это экран авторизации");
         AuthorizationScreen.authorization.check(matches(isDisplayed()));
     }
 
     public void enterLogin(String text) {
+         Allure.step("Ввод логина '" + text + "'");
         AuthorizationScreen.login.check(matches(isEnabled()));
         AuthorizationScreen.login.perform(replaceText(text));
     }
 
     public void enterPassword(String text) {
+        Allure.step("Ввод пароля '" + text + "'");
         AuthorizationScreen.password.check(matches(isEnabled()));
         AuthorizationScreen.password.perform(replaceText(text));
     }
 
     public void signIn() {
+        Allure.step("Нажатие кнопки входа");
         AuthorizationScreen.buttonSignIn.check(matches(isClickable()));
         AuthorizationScreen.buttonSignIn.perform(click());
     }
