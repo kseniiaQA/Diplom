a9fa82f349d8b66e0708acc6196db21caa1b77d7
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-09 11:09:28 +0400

    Update MainSteps.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/MainSteps.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/MainSteps.java
@@ -9,7 +9,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibilit
 import static org.hamcrest.CoreMatchers.not;
 
 import android.os.SystemClock;
-
+ 
 import androidx.test.espresso.matcher.ViewMatchers;
 
 import ru.iteco.fmhandroid.ui.elements.MainScreen;
@@ -18,61 +18,74 @@ public class MainSteps {
     MainScreen MainScreen = new MainScreen();
 
     public void isMainScreen() {
+        Allure.step("убедиться что это главная страница");
         MainScreen.allNews.check(matches(isDisplayed()));
         MainScreen.allClaims.check(matches(isDisplayed()));
     }
 
     public void expandAllNews() {
+        Allure.step("Развернуть все новости");
         MainScreen.expandAllNews.check(matches(isDisplayed()));
         MainScreen.expandAllNews.perform(click());
     }
 
     public void expandAllClaims() {
+        Allure.step("Развернуть все объявления");
         MainScreen.expandClaims.check(matches(isDisplayed()));
         MainScreen.expandClaims.perform(click());
     }
 
     public void allNewsNotDisplayed() {
+        Allure.step("Проверить непоказанные новости");
         MainScreen.allNews.check(matches(not(isDisplayed())));
     }
 
     public void allNewsDisplayed() {
+        Allure.step("Проверить все отображенные новости");
         MainScreen.allNews.check(matches(isDisplayed()));
     }
 
     public void allClaimsNotDisplayed() {
+        Allure.step("Проверить неразвернутые объявления");
         MainScreen.allClaims.check(matches(not(isDisplayed())));
     }
 
     public void allClaimsDisplayed() {
+        Allure.step("Проверить отображенные объявления");
         MainScreen.allClaims.check(matches(isDisplayed()));
     }
 
     public void openAllNews() {
+        Allure.step("Открыть все новости");
         MainScreen.allNews.check(matches(isDisplayed()));
         MainScreen.allNews.perform(click());
     }
 
     public void openAllClaims() {
+        Allure.step("Открыть все объявления");
         MainScreen.allClaims.check(matches(isDisplayed()));
         MainScreen.allClaims.perform(click());
     }
 
     public void expandSingleNews() {
+        Allure.step("Развернуть одну новость);
         MainScreen.expandSingleNews.perform(actionOnItemAtPosition(0, click()));
         MainScreen.newsDescription.check(matches(isDisplayed()));
     }
 
     public void collapseSingleNews() {
+        Allure.step("Закрыть одну новость");
         MainScreen.categoryIcon.perform(click());
         MainScreen.newsDescriptionAfterCollapse.check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
     public void openSingleClaim() {
+        Allure.step("Развернуть одно объявление");
         MainScreen.firstClaimExecutorName.perform(click());
     }
 
-    public void createClaim() {
+    public void createClaim() { 
+        Allure.step("Создать объяление");
         MainScreen.addNewClaimButton.perform(click());
         SystemClock.sleep(1000);
     }
