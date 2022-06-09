b5324b13e4ee50ae2c988bdcf7b48e22738fcbaa
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-09 10:54:10 +0400

    Update CommonSteps.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/CommonSteps.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/CommonSteps.java
@@ -9,31 +9,38 @@ public class CommonSteps {
     CommonElements CommonElements = new CommonElements();
 
     public void logout() {
-        CommonElements.manImage.perform(click());
+        Allure.step("разлогиниться из приложения");
+        CommonElements.manImage.perform(click()); 
         CommonElements.exitButton.perform(click());
     }
 
     public void clickSave() {
+        Allure.step("Нажать "Сохранить"); 
         CommonElements.buttonSave.perform(click());
     }
 
     public void clickOK() {
+        Allure.step("Нажать "ок"); 
         CommonElements.buttonOkText.perform(click());
     }
 
     public void clickCancel() {
+         Allure.step("Нажать "Отменить"); 
         CommonElements.buttonCancel.perform(click());
     }
 
     public void clickCancelText() {
+         Allure.step("Нажать "Отменить текст"); 
         CommonElements.buttonCancelText.perform(click());
     }
 
     public void goToThematicQuotes() {
+         Allure.step("Перейти в цитаты"); 
         CommonElements.thematicQuotes.perform(click());
     }
 
-    public void goToScreen(String screen) {
+    public void goToScreen(String screen) { 
+         Allure.step("Перейти на экран"); 
         CommonElements.mainMenu.perform(click());
         switch (screen) {
             case ("Main"):
