5dd784518e9261bcbb3f671e7bf7689df5539a3c
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-09 11:13:15 +0400

    Update NewsSteps.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/NewsSteps.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/NewsSteps.java
@@ -12,36 +12,44 @@ public class NewsSteps {
     NewsScreen NewsScreen = new NewsScreen();
 
     public void isNewsScreen() {
+        Allure.step("Удиться что это раздел новостей");
         NewsScreen.news.check(matches(isDisplayed()));
         NewsScreen.buttonSort.check(matches(isDisplayed()));
     }
 
     public String getFirstNewsTitle() {
+        Allure.step("Получить заголовок первой новости");
         return Utils.TextHelpers.getText(NewsScreen.firstNews);
     }
 
     public String getLastNewsTitle() {
+        Allure.step("Получить заголовок последней новости");
         return Utils.TextHelpers.getText(NewsScreen.lastNews);
     }
 
     public String getFirstNewsAgainTitle() {
+        Allure.step("Получить заголовок первой новости снова");
         return Utils.TextHelpers.getText(NewsScreen.firstNewsAgain);
     }
 
     public void clickSortButton() {
+        Allure.step("Кликнуть отсортировать");
         NewsScreen.buttonSort.perform(click());
     }
 
     public void goToControlPanel() {
+        Allure.step("Перейти на главную панель");
         NewsScreen.buttonControlPanel.perform(click());
         NewsScreen.controlPanel.check(matches(isDisplayed()));
     }
 
     public void openFilter() {
+        Allure.step("Открыть фильтр");
         NewsScreen.buttonFilter.perform(click());
     }
 
     public void checkFirstNewsDate(String text) {
+        Allure.step("Проверить дату первой новости");
         NewsScreen.firstNewsDate.check(matches(withText(text)));
     }
 
