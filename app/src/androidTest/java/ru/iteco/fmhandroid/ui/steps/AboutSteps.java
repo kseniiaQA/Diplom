0c40b171ede3542a621ef26f0865308808255ee3
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-28 11:50:06 +0400

    Update AboutSteps.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AboutSteps.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/steps/AboutSteps.java
@@ -6,7 +6,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-
+import io.qameta.allure.kotlin.Allure;
 import ru.iteco.fmhandroid.ui.elements.AboutScreen;
 
 public class AboutSteps {
