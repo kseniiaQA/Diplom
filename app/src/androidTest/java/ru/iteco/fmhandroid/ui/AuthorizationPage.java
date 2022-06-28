fa15ed569f757ceccb6039bed5a0249032c0467e
kseniiaQA <82049513+kseniiaQA@users.noreply.github.com>
2022-06-28 11:48:58 +0400

    Update AuthorizationPage.java

--- app/src/androidTest/java/ru/iteco/fmhandroid/ui/AuthorizationPage.java
+++ app/src/androidTest/java/ru/iteco/fmhandroid/ui/AuthorizationPage.java
@@ -17,7 +17,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.junit4.DisplayName;
 import io.qameta.allure.android.runners.AllureAndroidJUnit4;
 import ru.iteco.fmhandroid.R;
 import ru.iteco.fmhandroid.ui.steps.AuthorizationSteps;
